ab4a41b27d78e9ace8f164eccf827ecdcb2aff81
NiubilityDiu <ma3tian1wen2@emory.edu>
2025-10-16 22:52:07 -0400

    week-08 jupyter notebook and python script

---
+++ week_08_main_1.py
@@ -0,0 +1,12 @@
+# Import your packages, modules, global constants first
+import self_py_fun.week_08_example_fun as ef
+import numpy as np
+
+
+# Call variables
+print(ef.alpha)
+
+# Call your functions
+print(ef.message_hello('Tianwen'))
+print(ef.fn_cubic(3))
+print(np.array([1.0]))
---
+++ week_08_main_2.py
@@ -0,0 +1,12 @@
+# Import your packages, modules, global constants first
+from self_py_fun.week_08_example_fun import *
+import numpy as np
+
+# Call your variable
+print(alpha)
+
+# Call your functions
+# Since you import everything, we do not have to write ef.xxx
+print(message_hello('Tianwen'))
+print(fn_cubic(3))
+print(np.array([1.0]))
