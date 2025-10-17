96ac5655f5ec1e1b6abefd5ce3fc215e88b42836
NiubilityDiu <ma3tian1wen2@emory.edu>
2025-10-16 23:02:22 -0400

    update header

---
+++ self_py_fun/__init__.py
@@ -0,0 +1,3 @@
+# Usually, we will have an empty header file.
+# You will put all your self-written functions under self_py_fun folder.
+# Of course, when you develop your own projects, you can give it another name.
